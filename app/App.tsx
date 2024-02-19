b5c9b6a601ab2ac2dd284289de96f124d4352a8b
jesdone <jesdone@gmail.com>
2024-02-19 12:25:38 -0500

    overhaul

---
+++ app/App.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import {
+  BrowserRouter,
+  NavLink,
+  Route,
+  Switch,
+} from "react-server-dom-webpack/server.edge";
+import styled from "styled-components";
+import TaskContext from "./contexts/task-store";
+import useLocalStorage from "./hooks/use-local-storage";
+import FocusScreen from "./screens/FocusScreen";
+import ListScreen from "./screens/ListScreen";
+import { colors, GlobalStyle } from "./styles";
+import { Task } from "./types";
+
+const Layout = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  padding: 35px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  margin-bottom: 45px;
+`;
+
+const TabButton = styled(NavLink)`
+  align-items: center;
+  background: #000;
+  color: #fff;
+  display: flex;
+  height: 62px;
+  justify-content: center;
+  text-decoration: none;
+  width: 120px;
+
+  &:first-child {
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+  }
+
+  &:last-child {
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+  }
+
+  &.active {
+    background: ${colors.primary};
+    color: #000;
+  }
+`;
+
+function App() {
+  const [tasks, setTasks] = useLocalStorage<Task[]>("tasks", []);
+
+  return (
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <TaskContext.Provider value={[tasks, setTasks]}>
+          <Layout>
+            <Nav>
+              <TabButton exact to="/" activeClassName="active">
+                List
+              </TabButton>
+              <TabButton to="/focus" activeClassName="active">
+                Focus
+              </TabButton>
+            </Nav>
+
+            <Switch>
+              <Route exact path="/">
+                <ListScreen />
+              </Route>
+              <Route path="/focus">
+                <FocusScreen />
+              </Route>
+            </Switch>
+          </Layout>
+        </TaskContext.Provider>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
